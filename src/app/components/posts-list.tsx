d4aec13b757c3a9aca2e07dece4575df6b7a2416
Dixit Parmar <dparmar6698@gmail.com>
2024-08-04 23:51:03 +0530

    Pinned the create post top of the feed

--- src/app/components/posts-list.tsx
+++ src/app/components/posts-list.tsx
@@ -43,59 +43,60 @@ export function PostsList() {
     );
   if (status === "error") return <p>Error: {error.message}</p>;
   return (
-    <ScrollArea className="h-screen">
+    <>
       <div className="flex flex-col gap-2 p-4 pt-0">
         <CreatePost />
       </div>
-      <div className="flex flex-col gap-2 p-4 pt-0">
-        {data.pages.map((group, i) => (
-          <Fragment key={i}>
-            {group.data.posts.map((post) => (
-              <button
-                key={post._id.toString()}
-                className={cn(
-                  "flex flex-col items-start gap-2 rounded-lg border p-3 text-left text-sm transition-all hover:bg-accent",
-                  selectedPost.selected === post._id && "bg-muted"
-                )}
-                onClick={() =>
-                  setSelectedPost({
-                    ...selectedPost,
-                    selected: post._id,
-                  })
-                }
-              >
-                <div className="flex w-full flex-col gap-1">
-                  <div className="flex items-center">
-                    <div className="flex items-center gap-2">
-                      <div className="font-semibold">
-                        {post.user.firstName} {post.user.lastName}
-                      </div>
-                      {/* {!item.read && (
+      <ScrollArea className="h-screen">
+        <div className="flex flex-col gap-2 p-4 pt-0">
+          {data.pages.map((group, i) => (
+            <Fragment key={i}>
+              {group.data.posts.map((post) => (
+                <button
+                  key={post._id.toString()}
+                  className={cn(
+                    "flex flex-col items-start gap-2 rounded-lg border p-3 text-left text-sm transition-all hover:bg-accent",
+                    selectedPost.selected === post._id && "bg-muted"
+                  )}
+                  onClick={() =>
+                    setSelectedPost({
+                      ...selectedPost,
+                      selected: post._id,
+                    })
+                  }
+                >
+                  <div className="flex w-full flex-col gap-1">
+                    <div className="flex items-center">
+                      <div className="flex items-center gap-2">
+                        <div className="font-semibold">
+                          {post.user.firstName} {post.user.lastName}
+                        </div>
+                        {/* {!item.read && (
                     <span className="flex h-2 w-2 rounded-full bg-blue-600" />
                   )} */}
+                      </div>
+                      <div
+                        className={cn(
+                          "ml-auto text-xs",
+                          selectedPost.selected === post._id
+                            ? "text-foreground"
+                            : "text-muted-foreground"
+                        )}
+                      >
+                        {formatDistanceToNow(new Date(post.createdAt), {
+                          addSuffix: true,
+                        })}
+                      </div>
                     </div>
-                    <div
-                      className={cn(
-                        "ml-auto text-xs",
-                        selectedPost.selected === post._id
-                          ? "text-foreground"
-                          : "text-muted-foreground"
-                      )}
-                    >
-                      {formatDistanceToNow(new Date(post.createdAt), {
-                        addSuffix: true,
-                      })}
+                    <div className="text-xs font-medium">
+                      Community
+                      {/* {item.subject} */}
                     </div>
                   </div>
-                  <div className="text-xs font-medium">
-                    Community
-                    {/* {item.subject} */}
+                  <div className="line-clamp-2 text-xs text-muted-foreground">
+                    {post.content.substring(0, 300)}
                   </div>
-                </div>
-                <div className="line-clamp-2 text-xs text-muted-foreground">
-                  {post.content.substring(0, 300)}
-                </div>
-                {/* {item.labels.length ? (
+                  {/* {item.labels.length ? (
               <div className="flex items-center gap-2">
                 {item.labels.map((label) => (
                   <Badge key={label} variant={getBadgeVariantFromLabel(label)}>
@@ -104,27 +105,28 @@ export function PostsList() {
                 ))}
               </div>
             ) : null} */}
-              </button>
-            ))}
-          </Fragment>
-        ))}
-      </div>
-      <div className="block" ref={ref}></div>
-      {(isFetching || isFetchingNextPage) && (
-        <div>
-          {Array.from({ length: 6 }).map((_, index) => (
-            <Skeleton key={index} className="h-[100px] rounded-xl m-4" />
+                </button>
+              ))}
+            </Fragment>
           ))}
         </div>
-      )}
-      <div className="w-full flex justify-center py-24">
-        {!hasNextPage && (
-          <h2 className="text-2xl text-center font-semibold">
-            Here, take this tropy for your thumb, It runs too much on social
-            media. <br /> <br /> 🏆
-          </h2>
+        <div className="block" ref={ref}></div>
+        {(isFetching || isFetchingNextPage) && (
+          <div>
+            {Array.from({ length: 6 }).map((_, index) => (
+              <Skeleton key={index} className="h-[100px] rounded-xl m-4" />
+            ))}
+          </div>
         )}
-      </div>
-    </ScrollArea>
+        <div className="w-full flex justify-center py-24">
+          {!hasNextPage && (
+            <h2 className="text-2xl text-center font-semibold">
+              Here, take this tropy for your thumb, It runs too much on social
+              media. <br /> <br /> 🏆
+            </h2>
+          )}
+        </div>
+      </ScrollArea>
+    </>
   );
 }
